673bf88f5366a8b36eec43fb17517f023cf4106a
avagyanvahe <appjam@dhcp-v195-078.cv.reshsg.uci.edu>
2013-11-21 18:32:45 -0800

    added classes for the different view controllers and added a save page

---
+++ FinalSay4/FinalSay4/FS3ViewControllerLongTermAssistance.m
@@ -0,0 +1,38 @@
+//
+//  FS3ViewControllerLongTermAssistance.m
+//  FinalSay4
+//
+//  Created by App Jam on 11/21/13.
+//  Copyright (c) 2013 Team7. All rights reserved.
+//
+
+#import "FS3ViewControllerLongTermAssistance.h"
+
+@interface FS3ViewControllerLongTermAssistance ()
+
+@end
+
+@implementation FS3ViewControllerLongTermAssistance
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FinalSay4/FinalSay4/FS3ViewControllerResuscitation.h
@@ -0,0 +1,13 @@
+//
+//  FS3ViewControllerResuscitation.h
+//  FinalSay4
+//
+//  Created by App Jam on 11/21/13.
+//  Copyright (c) 2013 Team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FS3ViewControllerResuscitation : UIViewController
+
+@end
